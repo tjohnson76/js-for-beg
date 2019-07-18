f3151c88d34040b78619eba88ae0b9f4ffd7d327
tjohnson <tjohnson@apls.state.al.us>
2019-07-17 23:24:02 -0500

    app complete

--- js/app.js
+++ js/app.js
@@ -1,20 +1,30 @@
-
-$('.search-button').click(function () {
-    alert("Button clicked");
-
-
-})       //triggering event to the search-button class
-
 var displayWikipediaData = function() {
+
+    var $linksElement = $('#links');
+    var searchString = $('#searchString').val();
+    var wikipediaUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchString + "&format=json&callback=wikiCallback";
+
     $.ajax({ 
-        url:
+        url: wikipediaUrl,
+        dataType: "jsonp",
+        jsonp: "callback",
         success: function(response) {
-            //do something
-            console.log("Yay! Success! Our ajax request worked!")
+            var linkLists = response[1];
+            //do something -- output the results of the Wikipedia data onto the screen
+
+            linkLists.forEach(function(item) {
+                var url = 'https:/en.wikipedia.org/wiki/' + item;
+                $linksElement.append('<li> <a href="' + url + '">' + item + '</a></li>');
+
+                return url;     
+            })
 
         }
 
+    });
 
-    })
+    return false;
 
 }
+
+$('#form').submit(displayWikipediaData);
